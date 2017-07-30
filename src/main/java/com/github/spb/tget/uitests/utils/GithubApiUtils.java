4142c321a5468cc328798ae697cf670126bf089b
tgetmanova <tatyana.getmanova@gmail.com>
2017-07-31 01:31:07 +0300

    GithubRepo entity from json deserialization

--- src/main/java/com/github/spb/tget/uitests/utils/GithubApiUtils.java
+++ src/main/java/com/github/spb/tget/uitests/utils/GithubApiUtils.java
@@ -1,15 +1,23 @@
 package com.github.spb.tget.uitests.utils;
 
+import com.github.spb.tget.uitests.utils.data.GithubRepository;
+import com.google.gson.Gson;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Base64;
 
 public class GithubApiUtils {
-    public static void getRepos() {
+
+    public static Gson gson = new Gson();
+
+    public static GithubRepository[] getRepositories() {
         String encoding = Base64.getEncoder().encodeToString(
                 String.format("%s:%s", UserContext.getLogin(), UserContext.getPassword()).getBytes());
 
@@ -21,6 +29,13 @@ public class GithubApiUtils {
 
         try {
             HttpResponse response = client.execute(getRequest);
+            InputStream input = response.getEntity().getContent();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+                return gson.fromJson(reader, GithubRepository[].class);
+            } catch (Throwable throwable) {
+                throw new IllegalStateException(String.format("Cannot read single item from content: %s", throwable.getMessage()));
+            }
         } catch (IOException ioe) {
             throw new RuntimeException(ioe);
         }
---
+++ src/main/java/com/github/spb/tget/uitests/utils/data/GithubRepository.java
@@ -0,0 +1,28 @@
+package com.github.spb.tget.uitests.utils.data;
+
+public class GithubRepository {
+    private int id;
+    private String name;
+    private String full_name;
+    private Owner owner;
+
+    private class Owner {
+        private String login;
+        private int id;
+        private String avatar_url;
+        private String gravatar_id;
+        private String url;
+        private String html_url;
+        private String followers_url;
+        private String following_url;
+        private String gists_url;
+        private String starred_url;
+        private String subscriptions_url;
+        private String organizations_url;
+        private String repos_url;
+        private String events_url;
+        private String received_events_url;
+        private String type;
+        private Boolean site_admin;
+    }
+}
