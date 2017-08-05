1387f8fa4998b785ceb4bed09917089e5ae1b2a1
tgetmanova <tatyana.getmanova@gmail.com>
2017-08-05 20:43:09 +0300

    OsContext improvements

--- src/main/java/com/github/spb/tget/uitests/environment/OsContext.java
+++ src/main/java/com/github/spb/tget/uitests/environment/OsContext.java
@@ -1,9 +1,19 @@
 package com.github.spb.tget.uitests.environment;
 
+import java.util.Arrays;
+
 public class OsContext {
 
     public static String getOsType() {
-        String osType = System.getProperty("os.type");
-        return osType != null ? osType : "windows";
+        String osName = System.getProperty("os.name").toLowerCase();
+
+        if (osName.contains("win")) {
+            return "windows";
+        } else if (osName.contains("mac")) {
+            return "macos";
+        } else if (Arrays.stream(new String[]{"nix", "nux", "aix"}).anyMatch(i -> osName.contains(i))) {
+            return "linux";
+        }
+        throw new IllegalStateException("Cannot identify Operating System type: " + osName);
     }
 }
